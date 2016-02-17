eae8a37131542de355fa6eda77ec333227573062
Zhe Feng <ZFeng@lan.creditcorp.com.au>
2016-02-17 12:02:46 +1100

    remove jquery

--- libs/Admin/components/App.js
+++ libs/Admin/components/App.js
@@ -12,7 +12,6 @@ class Admin extends Component {
         <div>
           <ul>
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/jquery">jquery</Link></li>
             <li><Link to="/moment">moment</Link></li>
             <li><Link to="/moduleA">moduleA</Link></li>
           </ul>
